a2cf04537a9c31efb7a9d3c3f6be5d91ad41cb39
valentin <val.pukhov@gmail.com>
2024-03-08 10:22:00 +0100

    Init

---
+++ app/src/main/java/de/hhufscs/campusguesser/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package de.hhufscs.campusguesser.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import de.hhufscs.campusguesser.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
