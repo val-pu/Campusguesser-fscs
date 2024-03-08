5c2ea1dddc99872a0aba7c6970b6c78e7438f300
valentin <val.pukhov@gmail.com>
2024-03-08 10:33:07 +0100

    Added osmdroid dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    implementation("org.osmdroid:osmdroid-android:6.1.18")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
