20db29f8079e01d8e1be796d3ddc32cd2a47109d
valentin <val.pukhov@gmail.com>
2024-03-08 10:28:12 +0100

    Rename Activity

--- app/src/main/java/de/hhufscs/campusguesser/ui/GuessActivity.kt
+++ app/src/main/java/de/hhufscs/campusguesser/ui/GuessActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import de.hhufscs.campusguesser.R
 
-class MainActivity : AppCompatActivity() {
+class GuessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_guess)
     }
 }
