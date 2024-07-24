24441855b2fd493e419fd619decd9878f04861ae
Leonard Delpy <leonard.delpy@gmail.com>
2024-07-24 23:21:34 +0200

    Added documentation for Level
    Basically only added documentation for the Level class (could be wrong,
    I'm new to the project) and a minor change inside the `guess` function
    because the was a variable indroduced which is unnecessary, so  removed
    it.
    
    modified:   app/src/main/java/de/hhufscs/campusguesser/core/Level.kt
    
    Author: Leonard Delpy <leonard.delpy@gmail.com>

--- app/src/main/java/de/hhufscs/campusguesser/core/Level.kt
+++ app/src/main/java/de/hhufscs/campusguesser/core/Level.kt
@@ -1,3 +1,15 @@
+/* de.hhufscs.campusguesser.core.Level
+
+This class is the base of CampusGuesser. It implements the levels inside the application.
+
+The class Level implements the interface ILevel. On construction, it gets a bifunction to calculate
+the point for each guess and it gets a list of locations to guess. The bifunction is saved in the
+private variable `resultComputer`, the list of locations to guess in the private variable `guessStack`.
+All guessed spots are saved in the private variable `guessedList`, which contains a list of `GuessResults`
+containing the actual spot, the guessed spot and the points given. The points are saved separately
+inside Level inside the private variable `points`.
+*/
+
 package de.hhufscs.campusguesser.core
 
 import org.osmdroid.api.IGeoPoint
@@ -9,6 +21,8 @@ import kotlin.math.sqrt
 
 class Level : ILevel{
     companion object{
+        // TODO: Could implement another formula to calculate the distance which is more accurate in regard to take the earth curvature in account
+        // This would be nice, but I think it is not that relevant
         fun standardResultComputer(pointA: IGeoPoint, pointB: IGeoPoint) : Int {
             var distance: Double = sqrt((pointA.latitude - pointB.latitude).pow(2) + (pointA.longitude - pointB.longitude).pow(2))
             var points: Int = (100 / (1 + distance * 2300)).toInt()
@@ -16,12 +30,15 @@ class Level : ILevel{
         }
     }
 
-
     private var guessStack: Stack<IGuess>
     private var resultComputer: BiFunction<IGeoPoint, IGeoPoint, Int>
     private var guessedList: LinkedList<GuessResult>
     private var points: Int
 
+    /* constructor(resultComputer: BiFunction<IGeoPoint, IGeoPoint, Int>, guessesList: List<IGuess>)
+    Simply create the level and initialize all necessary variables. For more information on each variable
+    read the header of the file.
+    */
     constructor(resultComputer: BiFunction<IGeoPoint, IGeoPoint, Int>, guessesList: List<IGuess>){
         this.resultComputer = resultComputer
         this.guessStack = Stack<IGuess>()
@@ -38,10 +55,14 @@ class Level : ILevel{
         return guessStack.size >= 1
     }
 
+    /* guess(guessedSpot: IGeoPoint): GuessResult
+    Calculates the result of a guess based on the `guessedSpot` and the actual spot given inside the
+    stack of spots to guess. The guess result contains the actual spot, the guessed spot and the points
+    given in this order.
+    */
     override fun guess(guessedSpot: IGeoPoint): GuessResult {
-        var currentGuess: IGuess = getCurrentGuess()
-        var actualSpot: IGeoPoint = currentGuess.getLocation()
-        var points: Int = resultComputer.apply(guessedSpot, currentGuess.getLocation())
+        var actualSpot: IGeoPoint = getCurrentGuess().getLocation()
+        var points: Int = resultComputer.apply(guessedSpot, actualSpot)
         this.points += points
         var guessResult: GuessResult = GuessResult(actualSpot, guessedSpot, points)
         guessedList.add(guessResult)
