2a25f25e93fac9efb2118972509a5a97e70c2601
val-pu <val.pukhov@gmail.com>
2024-08-02 12:52:19 +0200

    Added ViewBindings to MenuActivity

--- app/src/main/java/de/hhufscs/campusguesser/ui/menu/MenuActivity.kt
+++ app/src/main/java/de/hhufscs/campusguesser/ui/menu/MenuActivity.kt
@@ -6,32 +6,37 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import de.hhufscs.campusguesser.R
 import de.hhufscs.campusguesser.creator.CreatorActivity
+import de.hhufscs.campusguesser.databinding.ActivityMenuBinding
+import de.hhufscs.campusguesser.databinding.ActivityMenuNewBinding
 import de.hhufscs.campusguesser.ui.GuessActivity
 
 class MenuActivity : AppCompatActivity() {
 
 
+    lateinit var binding: ActivityMenuNewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-        setContentView(R.layout.activity_menu_new)
-
-        findViewById<TextView>(R.id.btn_play_local).setOnClickListener {
-            val localIntent = Intent(applicationContext, GuessActivity::class.java)
-            localIntent.putExtra("online", false)
-            startActivity(localIntent)
+        binding = ActivityMenuNewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            btnPlayLocal.setOnClickListener {
+                val localIntent = Intent(applicationContext, GuessActivity::class.java)
+                localIntent.putExtra("online", false)
+                startActivity(localIntent)
+            }
+            btnPlayOnline.setOnClickListener {
+                val onlineIntent = Intent(applicationContext, GuessActivity::class.java)
+                onlineIntent.putExtra("online", true)
+                startActivity(onlineIntent)
+            }
+            btnEditor.setOnClickListener {
+                startActivity(Intent(applicationContext, CreatorActivity::class.java))
+            }
         }
 
-        findViewById<TextView>(R.id.btn_play_online).setOnClickListener {
-            val onlineIntent = Intent(applicationContext, GuessActivity::class.java)
-            onlineIntent.putExtra("online", true)
-            startActivity(onlineIntent)
-        }
 
-        findViewById<TextView>(R.id.btn_editor).setOnClickListener {
-            startActivity(Intent(applicationContext, CreatorActivity::class.java))
-        }
     }
 
 }
