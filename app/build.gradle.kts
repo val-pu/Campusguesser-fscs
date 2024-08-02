663f72b0776ca99f803ebc88ad34c08b50035767
val-pu <val.pukhov@gmail.com>
2024-08-02 11:08:42 +0200

    updated Dependencies and added Dependency for Progressbars

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,21 +46,23 @@ android {
 
 
 dependencies {
+
     implementation("org.osmdroid:osmdroid-android:6.1.18")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.activity:activity-ktx:1.9.1")
     implementation("io.github.shashank02051997:FancyToast:2.0.2")
-    implementation("androidx.activity:activity:1.8.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("androidx.activity:activity-ktx:1.9.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("com.jsibbold:zoomage:1.3.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     testImplementation("junit:junit:4.13.2")
     implementation("commons-io:commons-io:2.16.1")
+    implementation("com.github.skydoves:progressview:1.1.3")
 }
 
