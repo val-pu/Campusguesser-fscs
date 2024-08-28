c58dcace5b73e60c05f0f32a5a52d5e63e57fba7
val-pu <val.pukhov@gmail.com>
2024-08-28 22:38:27 +0200

    REBASE

--- app/src/main/java/de/hhufscs/campusguesser/ui/menu/MenuActivity.kt
+++ app/src/main/java/de/hhufscs/campusguesser/ui/menu/MenuActivity.kt
@@ -4,20 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import de.hhufscs.campusguesser.databinding.ActivityMenuNewBinding
-import de.hhufscs.campusguesser.creator.CreatorActivity
-import de.hhufscs.campusguesser.databinding.ActivityMenuBinding
 import de.hhufscs.campusguesser.ui.game.GuessActivity
-import de.hhufscs.campusguesser.ui.util.AnimatedPopup
 
 class MenuActivity : AppCompatActivity() {
 
 
-    lateinit var binding: ActivityMenuBinding
+    lateinit var binding: ActivityMenuNewBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMenuBinding.inflate(layoutInflater)
+        binding = ActivityMenuNewBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.apply {
             btnPlayLocal.setOnClickListener {
@@ -25,16 +22,7 @@ class MenuActivity : AppCompatActivity() {
                 localIntent.putExtra("online", false)
                 startActivity(localIntent)
             }
-            btnPlayOnline.setOnClickListener {
-                val onlineIntent = Intent(applicationContext, GuessActivity::class.java)
-                onlineIntent.putExtra("online", true)
-                startActivity(onlineIntent)
-            }
-            btnEditor.setOnClickListener {
-                startActivity(Intent(applicationContext, CreatorActivity::class.java))
-            }
         }
-
 
     }
 
